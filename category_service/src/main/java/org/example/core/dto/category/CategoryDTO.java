9eeccc8926c144ce2d996f16efb3fc5f1be07b2f
Ilya Dubenok <dubenokilya@gmail.com>
2023-10-28 23:50:35 +0300

    removed unnecessary imports

--- category_service/src/main/java/org/example/core/dto/category/CategoryDTO.java
+++ category_service/src/main/java/org/example/core/dto/category/CategoryDTO.java
@@ -1,6 +1,5 @@
 package org.example.core.dto.category;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- category_service/src/main/java/org/example/core/dto/rule/ExpirationRuleCreateDTO.java
+++ category_service/src/main/java/org/example/core/dto/rule/ExpirationRuleCreateDTO.java
@@ -1,7 +1,6 @@
 package org.example.core.dto.rule;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
