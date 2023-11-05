4b822da2748a8e47e954f8c453cf4d291922e180
Ilya Dubenok <dubenokilya@gmail.com>
2023-11-05 03:27:04 +0300

    added config server

--- category_service/build.gradle.kts
+++ category_service/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.google.guava:guava:11.0.2")
     implementation("org.modelmapper:modelmapper:3.2.0")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "project_1c"
 include("category_service")
 include("eureka_server")
 include("gateway")
+include("config_server")
