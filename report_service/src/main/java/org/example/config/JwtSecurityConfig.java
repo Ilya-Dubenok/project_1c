84996547592407fd87b436339b0517bc1c88af28
Ilya Dubenok <dubenokilya@gmail.com>
2023-11-26 22:21:45 +0300

    keycloak+db containers; kc init realm; report-service oauth2.0 configuration and sample of security auth mather

---
+++ report_service/src/main/java/org/example/config/JwtSecurityConfig.java
@@ -0,0 +1,31 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class JwtSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(authorization ->
+                authorization
+                        .requestMatchers("report/full")
+                        .hasAnyAuthority("SCOPE_perform_all_operations")
+                        .requestMatchers("report-service/v3/api-docs")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+
+}
