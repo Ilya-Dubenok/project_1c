cfeb736ab969659ff9a34e57f237510958815db1
Ilya Dubenok <dubenokilya@gmail.com>
2023-11-26 22:52:48 +0300

    changer report-service JwtSecurityConfig to allow delete operations only for perform_all_operations scope

--- report_service/src/main/java/org/example/config/JwtSecurityConfig.java
+++ report_service/src/main/java/org/example/config/JwtSecurityConfig.java
@@ -2,6 +2,7 @@ package org.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,7 +18,7 @@ public class JwtSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(authorization ->
                 authorization
-                        .requestMatchers("report/full")
+                        .requestMatchers(HttpMethod.DELETE,"report/all")
                         .hasAnyAuthority("SCOPE_perform_all_operations")
                         .requestMatchers("report-service/v3/api-docs")
                         .permitAll()
