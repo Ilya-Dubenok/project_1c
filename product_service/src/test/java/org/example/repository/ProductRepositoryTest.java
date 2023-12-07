1991ca583f1eca5b092e28d294847bc1b3c8b4a6
Ilya Dubenok <dubenokilya@gmail.com>
2023-12-07 20:22:23 +0300

    some tests for product repository added

--- product_service/src/test/java/org/example/repository/ProductRepositoryTest.java
+++ product_service/src/test/java/org/example/repository/ProductRepositoryTest.java
@@ -1,11 +1,18 @@
 package org.example.repository;
 
 import org.example.base.BaseRepositoryContainerTest;
+import org.example.dao.entities.product.Product;
 import org.example.dao.repository.IProductRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DuplicateKeyException;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootTest
 @DisplayName("Testing product repository")
@@ -14,10 +21,23 @@ public class ProductRepositoryTest extends BaseRepositoryContainerTest {
     @Autowired
     private IProductRepository repository;
 
-    @Test
-    public void test1() {
-
+    @AfterEach
+    public void deleteAllData() {
+        repository.deleteAll();
     }
 
+    @Test
+    public void findByNameWithIgnoreCase() {
+        String initName = "some name";
+        repository.save(new Product(UUID.randomUUID(), UUID.randomUUID(), initName, null, null));
+        Optional<Product> optionalProduct = repository.findByNameIgnoreCase("SoME NaME");
+        Assertions.assertEquals(initName, optionalProduct.orElseThrow().getName());
+    }
 
+    @Test
+    public void saveForExistingNameThroes() {
+        String sharedName = "some name";
+        repository.save(new Product(UUID.randomUUID(), UUID.randomUUID(), sharedName, null, null));
+        Assertions.assertThrows(DuplicateKeyException.class, () -> repository.save(new Product(UUID.randomUUID(), UUID.randomUUID(), sharedName, null, null)));
+    }
 }
